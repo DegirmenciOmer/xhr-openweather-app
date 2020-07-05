5761f032ce7ff55bf8a46042dcf81e9e94599d46
Omer Degirmenci <rumidegirmenci0@gmail.com>
2020-07-05 17:33:20 +0200

    some modifications ok, but needs alteration in javascript to reset input value

--- axios/app.js
+++ axios/app.js
@@ -1,11 +1,11 @@
+
+
 // Make a request for a user with a given ID
 
 const input = document.querySelector('input')
 
 const getWeather = (cityName) => {
   const resultText = document.querySelector('#resultText')
-
-
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=d60ebbc71990a2bf77fc7e5be1e6bb92&units=metric`
   axios.get(url)
   .then(function (response) {
@@ -13,17 +13,19 @@ const getWeather = (cityName) => {
     resultText.style.color = 'black'
     const weatherDescription = document.querySelector('#description');
     weatherDescription.innerHTML = `${response.data.weather[0].description} <img width="30px" src="file:///C:/Users/Gebruiker/Downloads/clouds.svg">`
-
   })
   .catch(function (error) {
     resultText.textContent = `Something went wrong... ${error}`
     resultText.style.color = 'red'
   })
+
 }
+
 input.onkeydown = function(e){
   if(e.key == 'Enter' ){
     const cityName = document.querySelector('#cityTextInput').value;
     getWeather(cityName);
+    
   }
 }
 document.querySelector('button').onclick = function(){
